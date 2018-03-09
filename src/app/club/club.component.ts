cbaab1cd01e8f1a3e83bcdffdb09f834d6206cfe
Chris Barnard <cbarnard@gmail.com>
2018-03-09 09:34:55 -0600

    Working on forms

--- src/app/club/club.component.ts
+++ src/app/club/club.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { LocalStorageService } from '../services/local-storage.service';
 import { Club } from '../models/club';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-club',
@@ -12,11 +14,32 @@ export class ClubComponent implements OnInit {
   @Output() newClub: EventEmitter<Club> = new EventEmitter();
   @Output() updatedClub: EventEmitter<Club> = new EventEmitter();
   @Input() currentClub: Club;
+  @Input() currentId: number;
   @Input() isEdit: boolean;
 
   constructor(private localStorageService: LocalStorageService) { }
 
   ngOnInit() {
+  }
+
+  addClub(club: any): Observable<Club> {
+    return Observable.create(o => {
+      let data: Club[] = JSON.parse(localStorage.getItem('clubs')).map(e => new Club(e))
+      let id = data[data.length - 1].id + 1
+      let added = new Club(Object.assign(club, { id: id }))
+      data.push(added)
+      localStorage.setItem('clubs', JSON.stringify(data))
+      o.next(added)
+      o.complete()
+    })
+  }
+
+  updatePost() {
+    this.localStorageService.editClub(this.currentId, this.currentClub).subscribe(club => {
+      console.log(club);
+      this.isEdit = false;
+      this.updatedClub.emit(club);
+    });
   }
 
 }
--- src/app/clubs/clubs.component.ts
+++ src/app/clubs/clubs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { Club } from '../models/club';
+import { ClubsService } from '../services/clubs.service';
 import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
@@ -8,14 +9,50 @@ import { LocalStorageService } from '../services/local-storage.service';
   styleUrls: ['./clubs.component.css']
 })
 export class ClubsComponent implements OnInit {
-  @Output() newClub: EventEmitter<Club> = new EventEmitter();
-  @Output() updatedClub: EventEmitter<Club> = new EventEmitter();
-  @Input() currentClub: Club;
-  @Input() isEdit: boolean;
+  clubs: Club[];
+  currentClub: Club = {
+    id: 0,
+    name: '',
+    city: '',
+    color: 1,
+    isTopFour: false,
+    nextMatchDate: ''
+  }
+  isEdit: boolean = false;
 
-  constructor() { }
+  constructor(private localStorageService: LocalStorageService) { }
 
   ngOnInit() {
+    this.localStorageService.getClubs().subscribe(clubs => {
+      this.clubs = clubs;
+    });
+  }
+
+  onNewClub(club: Club) {
+    this.clubs.unshift(club);
+  }
+
+  editClub(club: Club) {
+    this.currentClub = club;
+    this.isEdit = true;
+  }
+
+  onUpdatedclub(club: Club) {
+    this.clubs.forEach((cur, index) => {
+      if(club.id === cur.id) {
+        this.clubs.splice(index, 1);
+        this.clubs.unshift(club);
+        this.isEdit = false;
+        this.currentClub = {
+          id: 0,
+          name: '',
+          city: '',
+          color: 1,
+          isTopFour: false,
+          nextMatchDate: ''
+        }
+      }
+    });
   }
 
 }
